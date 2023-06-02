3021b766ededd9e7a0ea76c785488cfe2dd6b395
ayush <ayush.chauhan0008@gmail.com>
2023-06-02 20:11:39 +0530

    configure store and create ui-slice

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,14 @@
 import ReactDOM from "react-dom/client";
 
+import { Provider } from "react-redux";
+
 import App from "./App.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+import store from "./store/index.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
