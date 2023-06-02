272e8f599db876fd8f8e5f9da2ccedf37f7b91ad
ayush <ayush.chauhan0008@gmail.com>
2023-06-02 12:35:48 +0530

    login/logout and conditional rendering

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,23 +1,42 @@
-import classes from './Auth.module.css';
+import { useDispatch } from "react-redux";
+
+import classes from "./Auth.module.css";
+import { authActions } from "../store/index";
 
 const Auth = () => {
-  return (
-    <main className={classes.auth}>
-      <section>
-        <form>
-          <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
-          </div>
-          <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
-          </div>
-          <button>Login</button>
-        </form>
-      </section>
-    </main>
-  );
+	// save the dispatch to a variable
+	const dispatch = useDispatch();
+
+	function loginhandler(e) {
+		e.preventDefault();
+
+		// call the login action from saved dispatch
+		dispatch(authActions.login());
+	}
+
+	return (
+		<main className={classes.auth}>
+			<section>
+				<form onSubmit={loginhandler}>
+					<div className={classes.control}>
+						<label htmlFor="email">Email</label>
+						<input
+							type="email"
+							id="email"
+						/>
+					</div>
+					<div className={classes.control}>
+						<label htmlFor="password">Password</label>
+						<input
+							type="password"
+							id="password"
+						/>
+					</div>
+					<button>Login</button>
+				</form>
+			</section>
+		</main>
+	);
 };
 
 export default Auth;
